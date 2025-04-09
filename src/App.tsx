dbff48b6782d8425f11020468b18661321f5bbf6
mageshkannan <mageshkannanam@gmail.com>
2025-04-09 18:38:44 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="bg-black/90 text-primary flex h-screen justify-center">
+      <h1 className="text-5xl font-bold mt-3 underline underline-offset-4">
+        Thoughtverse
+      </h1>
+    </main>
+  );
+}
+
+export default App;
